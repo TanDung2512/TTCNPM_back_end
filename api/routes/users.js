155a574830a6da41919b6a3c86ea7b0a9333730f
Nguyen Tan Dung <tandung251298@gmail.com>
2019-05-05 15:21:21 +0700

    fix bug

--- api/routes/users.js
+++ api/routes/users.js
@@ -10,3 +10,6 @@ router.post("/login",(req,res,next) => {
 router.post("/logup",(req,res,next) => {
 
 })
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./api/routes/users');
 const app = express();
 
 
@@ -12,11 +11,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 /*---------- route ----------*/
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', usersRouter);
 app.use('/search', usersRouter);
