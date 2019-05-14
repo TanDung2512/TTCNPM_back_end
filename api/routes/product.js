0452fbc13c862c3ba438c08434d87c72c55d712e
Alvin Nguyen <anhhungsongnguyen@gmail.com>
2019-05-15 00:41:33 +0700

    Implemented product management page

--- api/routes/product.js
+++ api/routes/product.js
@@ -41,8 +41,32 @@ router.get('/page/:page', cors(), (req, res, next) => {
             length: result.length,
             data: result
         }
+        res.send(response)
+    })
+})
+
+router.get('/search/:search', cors(), (req, res, next) => {
+    con.query(`SELECT * FROM products WHERE product_name LIKE '${req.params.search}%'`, (err, result, fields) => {
+        if(err) throw err
+        let response = {
+            length: result.length,
+            numOfPages: result.length % 10 == 0 ? Math.floor(result.length/10) : Math.floor(result.length/10) + 1,
+            data: result
+        }
         console.log(response)
         res.send(response)
+    })
+})
+
+router.post('/create', cors(), (req, res, next) => {
+    con.query(`INSERT INTO products (product_name, product_type, product_brand, product_category, product_amount, product_price, product_description) 
+    VALUES ('${req.body.name}', '${req.body.type}', '${req.body.brand}', '${req.body.category}', '${req.body.amount}', '${req.body.price}', '${req.body.desc}')`, (err, result, fields) => {
+        if(err) {
+            res.send("CREATE UNSUCCESSFUL")
+        }
+        else{
+            res.send("CREATE SUCCESSFUL")
+        }
     })
 })
 
